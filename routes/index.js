6223b0498cbadc4249dadb04d31b7812b19c05ae
ajoy kumar sinha <ajoykumarsinha@gmail.com>
2019-01-17 14:24:39 +0530

    zvv

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@
 /*
  * GET home page.
  */
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'eu-west-1'});
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
